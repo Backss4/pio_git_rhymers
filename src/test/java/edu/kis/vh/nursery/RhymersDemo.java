55f7af0353064685e3845eef60e6734cb9486a9b
228902 <228902@CARBON.CTI.P.LODZ.PL>
2021-12-20 11:34:38 +0100

    constants and annotations added

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,27 +5,33 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
 
+    public static final int ONE = 1;
+    public static final int ZERO = 0;
+    public static final int MAX_I = 15;
+    public static final int MAX_J = 3;
+    public static final String REJECT_CAPTION = "total rejected is ";
+
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
 
         DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = ONE; i < MAX_I; i++)
+            for (int j = ZERO; j < MAX_J; j++)
                 rhymers[j].countIn(i);
 
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
+        for (int i = ONE; i < MAX_I; i++)
             rhymers[3].countIn(rn.nextInt(20));
 
-        for (int i = 0; i < rhymers.length; i++) {
+        for (int i = ZERO; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
 
-        System.out.println("total rejected is "
+        System.out.println(REJECT_CAPTION
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
 
     }
