4a25643512ded3162d036b3547addc238aefa81e
backs <26674182+Backss4@users.noreply.github.com>
2022-02-03 14:41:41 +0100

    2.3. Fabryki, diagramy, testy, *gwiazdki

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,17 +1,66 @@
 package edu.kis.vh.nursery;
 
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
+import edu.kis.vh.nursery.factory.ListRhymerFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
+import edu.kis.vh.nursery.factory.StackRhymerFactory;
 
 class RhymersDemo {
 
     public static void main(String[] args) {
-        RhymersFactory factory = new DefaultRhymersFactory();
-        testRhymers(factory);
-
+        testRhymers(new DefaultRhymersFactory());
+        testStackRhymers(new StackRhymerFactory());
+        testListRhymers(new ListRhymerFactory());
     }
 
     private static void testRhymers(RhymersFactory factory) {
+        System.out.println("Default Rhymers Test:");
+        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
+                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+
+        for (int i = 1; i < 15; i++)
+            for (int j = 0; j < 3; j++)
+                rhymers[j].countIn(i);
+
+        java.util.Random rn = new java.util.Random();
+        for (int i = 1; i < 15; i++)
+            rhymers[3].countIn(rn.nextInt(20));
+
+        for (int i = 0; i < rhymers.length; i++) {
+            while (!rhymers[i].callCheck())
+                System.out.print(rhymers[i].countOut() + "  ");
+            System.out.println();
+        }
+
+        System.out.println("total rejected is "
+                + ((HanoiRhymer) rhymers[3]).reportRejected());
+    }
+
+    private static void testStackRhymers(RhymersFactory factory) {
+        System.out.println("Array Stack Rhymers Test:");
+        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
+                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+
+        for (int i = 1; i < 15; i++)
+            for (int j = 0; j < 3; j++)
+                rhymers[j].countIn(i);
+
+        java.util.Random rn = new java.util.Random();
+        for (int i = 1; i < 15; i++)
+            rhymers[3].countIn(rn.nextInt(20));
+
+        for (int i = 0; i < rhymers.length; i++) {
+            while (!rhymers[i].callCheck())
+                System.out.print(rhymers[i].countOut() + "  ");
+            System.out.println();
+        }
+
+        System.out.println("total rejected is "
+                + ((HanoiRhymer) rhymers[3]).reportRejected());
+    }
+
+    private static void testListRhymers(RhymersFactory factory) {
+        System.out.println("List Rhymers Test:");
         DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
